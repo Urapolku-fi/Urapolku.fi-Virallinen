a98ee536d6611bb6573f5674e8ea66e90e64ef4c
SplinteeX <Jepsteri04@gmail.com>
2023-07-04 16:24:37 +0300

    Changed to react

---
+++ frontend/src/animations.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+  
+  export default observer;
