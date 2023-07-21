e91a96053173c08cb91029bd52d8dfb1459f8ec1
SplinteeX <Jepsteri04@gmail.com>
2023-07-22 00:56:25 +0300

    Login desktop

---
+++ frontend/src/components/pages/public/Login.js
@@ -0,0 +1,67 @@
+import Logo from '../../pictures/urapolku.png';
+import Facebook from '../../pictures/facebook-logo.png';
+import Google from '../../pictures/Google-logo.png';
+import Passframe from '../../pictures/pass-frame.png';
+
+import '../../css/login.css';
+
+function Login() {
+    return (
+        <section id="Login">
+            <div id="Left-side">
+                <div className="Logo-wrapper">
+                    <div className="Top">
+                        <img id="logo"src={Logo} alt=""/>
+                        <h2>Urapolku</h2>
+                    </div>
+
+                    </div>
+                <p>create a free account to land your dream job and connect with your ideal employers</p>
+            </div>
+            <div id="Right-side">
+                <div className="box">
+                <h2>Welcome back!</h2>
+                <div className="Signin-buttons">
+                    <div className="Google-signin">
+                        <button>
+                            <img src={Google} alt=""/>
+                            <p>Sign in with google</p>
+                        </button>
+                    </div>
+
+                    <div className="Facebook-signin">
+                        <button>
+                            <img src={Facebook} alt=""/>
+                            <p>Sign in with Facebook</p>
+                        </button>
+                    </div>
+                    </div>
+                    <div className="OR">
+                        <p>-OR-</p>
+                    </div>
+                    
+                    <div className="input-fields">
+                        <div className="Email-field">
+                            <input type="text" className="email" placeholder='Email address' />
+                        </div>
+                        <div className="pass-field">
+                            <input type="password" className="password" placeholder='Password'/>
+                            <button><img src={Passframe} alt=""/></button>
+                        </div>
+                        
+                    </div>
+                    <button className="Log-in">
+                        <p>Login</p>
+                    </button>
+                    <div className="Create-account">
+                        <p>Dont have an account?</p>
+                        <li><a href="#">Sign up</a></li>
+                    </div>
+            </div>
+
+            </div>
+        </section>
+
+    )
+}
+export default Login;
