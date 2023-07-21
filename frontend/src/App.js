9d54dd5c9bc166cb281aa54f0932a3ba16ffff18
SplinteeX <Jepsteri04@gmail.com>
2023-07-22 00:56:25 +0300

    Login desktop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from './components/pages/public/home';
+import Login from './components/pages/public/Login';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Login />
     </div>
   );
 }
