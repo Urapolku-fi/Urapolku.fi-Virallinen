7d85c318476e7414d345a84371175b2b828af41a
luimu64 <luimu@luimu.dev>
2023-07-22 13:18:01 +0300

    migration to vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
