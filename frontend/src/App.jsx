71d51790567447a6976194e77e29ce48b0a24e42
luimu64 <luimu@luimu.dev>
2023-07-22 17:39:35 +0300

    partially working navigation/routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
+import { NavBarLayout } from "./components/NavLayout";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <NavBarLayout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </NavBarLayout>
+    </BrowserRouter>
   );
 }
 
