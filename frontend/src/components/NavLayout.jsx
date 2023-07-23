b2238364ea7cecf036d0332204c52bf7fe60f28c
luimu64 <luimu@luimu.dev>
2023-07-23 16:28:48 +0300

    navbar and routing and mobile view done

--- frontend/src/components/NavLayout.jsx
+++ frontend/src/components/NavLayout.jsx
@@ -1,28 +1,64 @@
 import { useState, createContext, useContext } from "react";
 import MenuIcon from "../assets/menu.svg";
 import MenuCloseIcon from "../assets/close-menu.svg";
+import "./css/navLayout.css";
 
 const NavMenuContext = createContext(undefined);
+
+const navbarMenuItems = [
+  {
+    text: "Avoimet Työpaikat",
+    link: "/browse",
+  },
+  {
+    text: "Jätä työpaikkailmoitus",
+    link: "/query",
+  },
+  {
+    text: "Suosikit",
+    link: "/favorites",
+  },
+  {
+    text: "Rekisteröidy",
+    link: "/register",
+    styling: " button-filled",
+  },
+  {
+    text: "Kirjaudu",
+    link: "/login",
+    styling: " button-hollow",
+  },
+];
 
 const MenuButton = () => {
   const vizContext = useContext(NavMenuContext);
 
   return (
-    <div
-      onClick={() => vizContext?.setSideBarVisible(!vizContext?.sideBarVisible)}
-    >
+    <>
       {vizContext?.sideBarVisible ? (
-        <img src={MenuCloseIcon} />
+        <img
+          className="menu-burger-button"
+          onClick={() =>
+            vizContext?.setSideBarVisible(!vizContext?.sideBarVisible)
+          }
+          src={MenuCloseIcon}
+        />
       ) : (
-        <img src={MenuIcon} />
+        <img
+          className="menu-burger-button"
+          onClick={() =>
+            vizContext?.setSideBarVisible(!vizContext?.sideBarVisible)
+          }
+          src={MenuIcon}
+        />
       )}
-    </div>
+    </>
   );
 };
 
-const NavLink = (props) => {
+const NavLink = ({ styling = "", ...props }) => {
   return (
-    <a href={props.link} className="nav-link">
+    <a href={props.link} className={`nav-link${styling}`}>
       {props.text}
     </a>
   );
@@ -30,25 +66,6 @@ const NavLink = (props) => {
 
 const SideBar = () => {
   const vizContext = useContext(NavMenuContext);
-
-  const navbarMenuItems = [
-    {
-      text: "Home",
-      link: "/",
-    },
-    {
-      text: "Selaa",
-      link: "/browse",
-    },
-    {
-      text: "Kirjaudu",
-      link: "/login",
-    },
-    {
-      text: "Kirjaudu ulos",
-      link: "/logout",
-    },
-  ];
 
   return (
     <>
@@ -80,10 +97,25 @@ const NavBarLayout = (props) => {
         setSideBarVisible: setSideBarVisible,
       }}
     >
-      <header className="">
+      <header className="navbar">
+        <div className="nav-logo-wrapper">
+          <img className="nav-logo" src={"/pictures/urapolku.png"} />
+          <p>Urapolku</p>
+        </div>
+        <div className="nav-items-wrapper">
+          {navbarMenuItems.map((item) => (
+            <NavLink
+              key={item.link}
+              link={item.link}
+              text={item.text}
+              styling={item.styling}
+            />
+          ))}
+        </div>
+
         <MenuButton />
       </header>
-      <SideBar />
+      {/*<SideBar />*/}
       {props.children}
     </NavMenuContext.Provider>
   );
