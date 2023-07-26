ff69df392ab3d91b629de481ee15ad434e4364be
luimu64 <luimu@luimu.dev>
2023-07-26 20:47:31 +0300

    api base with working authentication

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = 3000
+
+const { auth } = require('express-oauth2-jwt-bearer');
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_BASE_URL,
+});
+
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
