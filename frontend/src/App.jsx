932fce2c78b181883c00a91d7a850585ae44cc9f
luimu64 <luimu@luimu.dev>
2023-07-26 13:54:59 +0300

    auth0 implementation done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import Signup from "./components/pages/Signup"
 import { NavBarLayout } from "./components/NavLayout";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBarLayout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Signup />} />
-        </Routes>
-      </NavBarLayout>
-    </BrowserRouter>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENTID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+      }}
+    >
+      <BrowserRouter>
+        <NavBarLayout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </NavBarLayout>
+      </BrowserRouter>
+    </Auth0Provider>
   );
 }
 
