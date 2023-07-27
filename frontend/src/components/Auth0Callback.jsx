f8e4ffae91f65f73701921e3d506018716b0b815
luimu64 <luimu@luimu.dev>
2023-07-27 22:53:37 +0300

    sending user info to backend

--- frontend/src/components/Auth0Callback.jsx
+++ frontend/src/components/Auth0Callback.jsx
@@ -1,13 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import { useFetch } from "../api/user";
 
 const CallbackPage = () => {
   const { isLoading, user } = useAuth0();
+  const { postWAuth } = useFetch();
 
   useEffect(() => {
-    console.log(user);
-  }, [user]);
+    if (user) {
+      const dbUserData = {
+        email: user.email,
+        name: user.name,
+        picture: user.picture,
+      };
+
+      postWAuth("/user/create", dbUserData);
+    }
+  }, [user, postWAuth]);
 
   return (
     <>
