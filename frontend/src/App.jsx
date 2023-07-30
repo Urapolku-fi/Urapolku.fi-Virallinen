0f7fea08bbae1a9faf8da9119483d1f5b384e052
SplinteeX <Jepsteri04@gmail.com>
2023-07-30 19:07:45 +0300

    Finished dropdown menus on profile completion screen 2 (still needs some functionality)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup"
 import { NavBarLayout } from "./components/NavLayout";
 import ProfileCompletionOne from "./components/pages/ProfileCompletionOne";
+import ProfileCompletionTwo from "./components/pages/ProfileCompletionTwo";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
           <Route path="/profileone" element={<ProfileCompletionOne />} />
+          <Route path="/profiletwo" element={<ProfileCompletionTwo />} />
         </Routes>
       </NavBarLayout>
     </BrowserRouter>
