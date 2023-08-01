3e4e0b7494231540545511199e025ee49f042776
luimu64 <luimu@luimu.dev>
2023-08-01 14:04:40 +0300

    redoing the backend filestructure

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
-
 const express = require('express')
 const cors = require('cors')
-const {expressjwt: jwt} = require('express-jwt');
-const expressJwtSecret = require('jwks-rsa');
+const userRoutes = require('./ src/api/routes/user')
 require('dotenv').config()
 
 const app = express()
@@ -12,23 +10,8 @@ app.use(cors())
 app.options('*', cors())
 app.use(express.json())
 
-const checkJwt = jwt({
-  secret: expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${process.env.AUTH0_BASE_URL}.well-known/jwks.json`,
-  }),
+app.use('/api/user', userRoutes);
 
-  // Validate the audience and the issuer
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_BASE_URL,
-  algorithms: ['RS256'],
-});
-
-app.post('/api/user/create', function(req, res) {
-  console.log(req.body)
-})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
