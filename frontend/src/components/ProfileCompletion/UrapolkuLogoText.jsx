93d93c92f99eda8c5bfb458a5890692f7bda223e
SplinteeX <Jepsteri04@gmail.com>
2023-08-03 16:16:21 +0300

    Third onboarding page ready

--- frontend/src/components/ProfileCompletion/UrapolkuLogoText.jsx
+++ frontend/src/components/ProfileCompletion/UrapolkuLogoText.jsx
@@ -1,18 +1,17 @@
 function urapolkuLogo() {
-    const display = {
-        display: 'flex',
-        gap: '5px',
-        alignItems: 'center',
-        justifyConent: 'center',
-        color: '#FFF',
-        float: "left",
-        marginBottom: "20px"
-    };
-    return (
-        <div style={display}>
-             <img src="pictures/urapolku.png" alt="" width="25px" height="25px"/>
-             <h2>Urapolku</h2>
-        </div>
-    )
+  const display = {
+    display: "flex",
+    gap: "5px",
+    alignItems: "center",
+    justifyConent: "center",
+    color: "#FFF",
+    float: "left",
+  };
+  return (
+    <div style={display}>
+      <img src="pictures/urapolku.png" alt="" width="25px" height="25px" />
+      <h2>Urapolku</h2>
+    </div>
+  );
 }
-export default urapolkuLogo
+export default urapolkuLogo;
