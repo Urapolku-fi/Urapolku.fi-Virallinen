2d31b685db26e66690a87c25ee1cbc38153c55db
SplinteeX <Jepsteri04@gmail.com>
2023-08-05 03:12:02 +0300

    Moved every file to its own places + finished the onboarding sites - Missing animations

---
+++ frontend/src/components/pages/ProfileCompletion/nextButton.jsx
@@ -0,0 +1,20 @@
+function NextButton() {
+  const floatLeft = {
+    display: "flex",
+    float: "left",
+  };
+  const FilledButton = {
+    borderRadius: "8px",
+    border: "1px solid rgba(132, 131, 131, 0.85)",
+    background: "#120A2F",
+    color: "#FFF",
+  };
+  return (
+    <div className="Next-Button" style={floatLeft}>
+      <button style={FilledButton}>
+        <p className="Button-text-content">Next</p>
+      </button>
+    </div>
+  );
+}
+export default NextButton;
