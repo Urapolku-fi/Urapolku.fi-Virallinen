9152389c3d33fb6b2b6fb82bd30e66f199e3aebf
SplinteeX <Jepsteri04@gmail.com>
2023-08-05 03:10:41 +0300

    Added upload your photo + functionalities to first onboarding

--- frontend/src/components/pages/ProfileCompletionOne.jsx
+++ frontend/src/components/pages/ProfileCompletionOne.jsx
@@ -1,18 +1,44 @@
-import LeftBar from "../ProfileCompletion/LeftBarProfile";
-import SkipButton from "../ProfileCompletion/SkipButton";
-import NextButton from "../ProfileCompletion/nextButton";
-import ProgressBar from "../ProfileCompletion/progressBar";
-import UrapolkuLogo from "../ProfileCompletion/UrapolkuLogoText";
+import LeftBar from "./ProfileCompletion/LeftBarProfile";
+import SkipButton from "./ProfileCompletion/SkipButton";
+import NextButton from "./ProfileCompletion/nextButton";
+import ProgressBar from "./ProfileCompletion/progressBar";
+import UrapolkuLogo from "./ProfileCompletion/UrapolkuLogoText";
+import React, { useState } from "react";
 import "../css/ProfileCompletionOne.css";
-const nextPage = () => {
-  window.location.href = "/profiletwo";
-};
+
 function ProfileCompletionOne() {
+  const [imageUrl, setImageUrl] = useState("/pictures/Camera-alt.png");
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const clickImageButton = () => {
+    // Trigger the hidden input file when the button is clicked
+    document.getElementById("upload-image").click();
+  };
+  const nextPage = () => {
+    // User placed data will save and move to next page
+    window.location.href = "/profiletwo";
+  };
+  const skipPage = () => {
+    // No data saved, just skipped
+    window.location.href = "/profiletwo";
+  };
   return (
     <div id="ProfileCompletion-Wrapper">
       <LeftBar />
       <div className="Profile-completion-one-right">
-        <SkipButton />
+        <div className="Skipbutton-wrapper" onClick={skipPage}>
+          <SkipButton />
+        </div>
         <div className="Header-logo">
           <UrapolkuLogo />
         </div>
@@ -24,7 +50,38 @@ function ProfileCompletionOne() {
           <div className="Main-wrapper">
             <div className="User-photos">
               <p>Upload your photos</p>
-              <input type="file" id="fileInput" />
+              <button onClick={clickImageButton}>
+                <img src={"pictures/Camera-alt.png"} alt="" />
+                <input
+                  type="file"
+                  id="upload-image"
+                  onChange={handleImageChange}
+                />
+              </button>
+              <div
+                className={`Circle ${
+                  imageUrl === "/pictures/Camera-alt.png" ? "centered" : ""
+                }`}
+              >
+                {imageUrl === "/pictures/Camera-alt.png" ? (
+                  <img
+                    src={imageUrl}
+                    alt=""
+                    id="User-uploaded-photo"
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                    }}
+                  />
+                ) : (
+                  <img
+                    src={imageUrl}
+                    alt=""
+                    id="User-uploaded-photo"
+                    style={{ width: "100%", height: "100%", border: "none" }}
+                  />
+                )}
+              </div>
             </div>
           </div>
           <div className="Description">
@@ -39,7 +96,11 @@ function ProfileCompletionOne() {
             </div>
           </div>
           <div className="Bottom">
-            <div className="Next-button-wrapper" onClick={nextPage}>
+            <div
+              className="Next-button-wrapper"
+              onClick={nextPage}
+              onChange={handleImageChange}
+            >
               <NextButton />
             </div>
             <ProgressBar />
