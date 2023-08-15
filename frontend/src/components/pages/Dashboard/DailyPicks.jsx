1534c501e9f0f2a743bbf7a0f1612b7c1f0df937
SplinteeX <Jepsteri04@gmail.com>
2023-08-15 09:26:44 +0300

    Dashboard components

---
+++ frontend/src/components/pages/Dashboard/DailyPicks.jsx
@@ -0,0 +1,12 @@
+const DailyPicks = () => {
+  <section className="Daily-pick-frame">
+    <h2>Daily Picks</h2>
+    <div className="Daily-Pick-Card">
+      <div className="Card-text-content">
+        <p>Job</p>
+        <p>Haluatko mukaan dynaamiseen tiimiin jossa myös etätyö onnistuu?</p>
+      </div>
+    </div>
+  </section>;
+};
+export default DailyPicks;
---
+++ frontend/src/components/pages/Dashboard/UrapolkuTitle.jsx
@@ -0,0 +1,9 @@
+const UrapolkuTitle = () => {
+  <section className="Urapolku-title">
+    <h1>Yhdellä haulla urapolulta löydät kaikki avoimet työpaikat.</h1>;
+    <p>
+      Urapolku on työnetsimisalusta, joka tarjoaa sinulle nopean ja tehokkaan
+      tavan löytää juuri sinulle sopivia työpaikkoja.
+    </p>
+  </section>;
+};
