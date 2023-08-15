fa84ca64b13ad8f8f1ba53d17a130e87d354a64f
SplinteeX <Jepsteri04@gmail.com>
2023-08-15 09:26:44 +0300

    Dashboard components

---
+++ frontend/src/components/pages/Dashboard/UrapolkuInfo.jsx
@@ -0,0 +1,15 @@
+const UrapolkuInfo = () => {
+  <section className="Urapolku-info-container">
+    <h2>Tietoa urapolusta</h2>
+    <p>
+      Etsitpä sitten töitä tai haluat löytää uusia työntekijöitä, me tarjoamme
+      sinulle innovatiivisen ratkaisun löytää juuri sen mitä etsit: olipa
+      kyseessä sitten uusi työpaikka, taitava osaaja, suunnanmuutos, unelmatyön
+      tavoittelu tai vaikka kesätyö. Urapolullta löydät suuren valikoiman
+      avoimia työpaikkoja, parhaat vinkit työnhakuun ja kiehtovimmat tarinat
+      työelämän koukeroista.
+    </p>
+    <button>Rekisteröidy</button>
+  </section>;
+};
+export default UrapolkuInfo;
