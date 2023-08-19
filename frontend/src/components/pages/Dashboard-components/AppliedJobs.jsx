c8c5f97017c60b21bd975b12b4bec6c6760d8b0a
SplinteeX <Jepsteri04@gmail.com>
2023-08-19 11:03:47 +0300

    Components more reusable + fully responsive

--- frontend/src/components/pages/Dashboard-components/AppliedJobs.jsx
+++ frontend/src/components/pages/Dashboard-components/AppliedJobs.jsx
@@ -1,65 +1,34 @@
+import React from "react";
 import "../../css/Dashboard/AppliedJobs.css";
-const AppliedJobs = () => {
+
+function AppliedJobsCard({ imageSrc, title, description, location }) {
+  return (
+    <div className="AppliedJobs-Card">
+      <div className="AppliedJobs-circle-image">
+        <img className="AppliedJobs-card-image" src={imageSrc} alt="" />
+      </div>
+      <div className="AppliedJobs-card-text-content">
+        <h2 className="AppliedJobs-card-title">{title}</h2>
+        <p>{description}</p>
+        {location && <p className="AppliedJobs-card-location">{location}</p>}
+      </div>
+    </div>
+  );
+}
+function AppliedJobs({ appliedJobs }) {
   return (
     <section className="AppliedJobs-frame">
       <h2 className="AppliedJobs-title">Applied Jobs</h2>
-      <div className="AppliedJobs-Card">
-        <div className="AppliedJobs-circle-image">
-          <img
-            className="AppliedJobs-card-image"
-            src={"/pictures/temp/Lowes.png"}
-            alt=""
+      <div className="AppliedJobs-Cards">
+        {appliedJobs.map((job, index) => (
+          <AppliedJobsCard
+            key={index}
+            imageSrc={job.imageSrc}
+            title={job.title}
+            description={job.description}
+            location={job.location}
           />
-        </div>
-        <div className="AppliedJobs-card-text-content">
-          <h2 className="AppliedJobs-card-title">Lowe's Companies, Inc</h2>
-          <p>Human Resources Services</p>
-          <p className="AppliedJobs-card-location">Helsinki, Finland</p>
-        </div>
-      </div>
-      <div className="AppliedJobs-Card">
-        <div className="AppliedJobs-circle-image">
-          <img
-            className="AppliedJobs-card-image"
-            src={"/pictures/temp/Crafting.png"}
-            alt=""
-          />
-        </div>
-        <div className="AppliedJobs-card-text-content">
-          <h2 className="AppliedJobs-card-title">
-            Crafting More Than Words: Unveiling the..
-          </h2>
-          <p>Article</p>
-        </div>
-      </div>
-      <div className="AppliedJobs-Card">
-        <div className="AppliedJobs-circle-image">
-          <img
-            className="AppliedJobs-card-image"
-            src={"/pictures/temp/Twitter-eclipse.png"}
-            alt=""
-          />
-        </div>
-        <div className="AppliedJobs-card-text-content">
-          <h2 className="AppliedJobs-card-title">
-            Why Twitter’s rebrand to ‘X’ feels shocking...{" "}
-          </h2>
-          <p>News</p>
-        </div>
-      </div>
-      <div className="AppliedJobs-Card">
-        <div className="AppliedJobs-circle-image">
-          <img
-            className="AppliedJobs-card-image"
-            src={"/pictures/temp/mcdonalds-eclipse.png"}
-            alt=""
-          />
-        </div>
-        <div className="AppliedJobs-card-text-content">
-          <h2 className="AppliedJobs-card-title">McDonald's</h2>
-          <p>Restaurant</p>
-          <p className="AppliedJobs-card-location">Chicago, Illinois</p>
-        </div>
+        ))}
       </div>
       <div className="AppliedJobs-View-all-recommendations">
         <img
@@ -68,9 +37,12 @@ const AppliedJobs = () => {
           width="16px"
           height="8px"
         />
-        <h2 className="AppliedJobs-footer-title">View all recommendations</h2>
+        <a href="/" className="AppliedJobs-footer-link">
+          <h2 className="AppliedJobs-footer-title">View all applied jobs</h2>
+        </a>
       </div>
     </section>
   );
-};
+}
+
 export default AppliedJobs;
--- frontend/src/components/pages/Dashboard-components/DailyPicks.jsx
+++ frontend/src/components/pages/Dashboard-components/DailyPicks.jsx
@@ -1,19 +1,35 @@
+import React from "react";
 import "../../css/Dashboard/DailyPicks.css";
-const DailyPicks = () => {
+
+function DailyPickCard({ title, description }) {
+  return (
+    <div className="Daily-Pick-Card">
+      <div className="Card-text-content">
+        <p>{title}</p>
+        <p className="Card-description">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function DailyPicks({ dailyPicks }) {
   return (
     <section className="Daily-pick-frame">
       <h2>Daily Picks</h2>
       <div className="Daily-pick-background">
-        <div className="Daily-Pick-Card">
-          <div className="Card-text-content">
-            <p>Job</p>
-            <p className="Card-description">
-              Haluatko mukaan dynaamiseen tiimiin jossa myös etätyö onnistuu?
-            </p>
-          </div>
+        <div className="Daily-pick-cards">
+          {dailyPicks.map((pick, index) => (
+            <div className="Daily-pick-card" key={index}>
+              <DailyPickCard
+                title={pick.title}
+                description={pick.description}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
-};
+}
+
 export default DailyPicks;
