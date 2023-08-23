c84e7a6ba809e7f58940c19b8d93a78c4f9753e9
luimu64 <luimu@luimu.dev>
2023-08-23 21:10:18 +0300

    small consistency fix

--- frontend/src/components/ProfileImageUpload.jsx
+++ frontend/src/components/ProfileImageUpload.jsx
@@ -1,25 +1,25 @@
-const styles = {
-  profileImageBanner: {
-    backgroundColor: "#F0F0F0",
-    height: "10rem",
-    borderRadius: ".5rem",
-    display: "grid",
-    placeItems: "center",
-  },
-  profileImageProfilePicture: {
-    backgroundColor: "#D9D9D9",
-    padding: "2rem",
-    borderRadius: "100%",
-    outline: "1px solid #4A4A4A",
-    translate: "2rem -3.5rem",
-  },
-  profileImageText: {
-    fontWeight: "200",
-    marginBottom: ".5rem",
-  },
-};
-
 const ProfileImageUpload = () => {
+  const styles = {
+    profileImageBanner: {
+      backgroundColor: "#F0F0F0",
+      height: "10rem",
+      borderRadius: ".5rem",
+      display: "grid",
+      placeItems: "center",
+    },
+    profileImageProfilePicture: {
+      backgroundColor: "#D9D9D9",
+      padding: "2rem",
+      borderRadius: "100%",
+      outline: ".1rem solid #4A4A4A",
+      translate: "2rem -3.5rem",
+    },
+    profileImageText: {
+      fontWeight: "200",
+      marginBottom: ".5rem",
+    },
+  };
+
   return (
     <div>
       <h3 style={styles.profileImageText}>Upload your photos</h3>
