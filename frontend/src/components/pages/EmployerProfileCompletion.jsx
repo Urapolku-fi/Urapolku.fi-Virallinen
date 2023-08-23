e74b3a5bbac8c064aecf2c935b6435f161a5ca1d
luimu64 <luimu@luimu.dev>
2023-08-23 22:46:27 +0300

    partially done employer onboarding page

--- frontend/src/components/pages/EmployerProfileCompletion.jsx
+++ frontend/src/components/pages/EmployerProfileCompletion.jsx
@@ -1,11 +1,37 @@
 import { GradientLeftBar } from "./ProfileCompletion/LeftBarProfile";
+import SkipButton from "./ProfileCompletion/SkipButton";
+import "../css/employerProfileCompletion.css";
+import { useNavigate } from "react-router-dom";
+import ProfileImageUpload from "../ProfileImageUpload";
+import TextArea from "../TextArea";
 
-function EmployerOnboarding() {
+function EmployerProfileCompletion() {
+  const navigate = useNavigate();
+
+  const skipPage = () => {
+    navigate("/empbrowse");
+  };
+
   return (
-    <div>
+    <div id="employer-profile-completion">
       <GradientLeftBar />
+      <div id="employer-profile-completion-right-bar">
+        <div id="employer-profile-completion-top">
+          <div>
+            <img src="/pictures/urapolku.png" />
+            <p>Urapolku</p>
+          </div>
+          <SkipButton onClick={skipPage} />
+        </div>
+        <h1>Tell us more about you</h1>
+        <ProfileImageUpload />
+        <TextArea
+          title="Description"
+          placeholder="Write a description of your business, it's core values, vision etc."
+        />
+      </div>
     </div>
   );
 }
 
-export default EmployerOnboarding;
+export default EmployerProfileCompletion;
