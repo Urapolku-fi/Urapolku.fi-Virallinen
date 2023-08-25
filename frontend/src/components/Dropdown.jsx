f9169e907987b9480042728fdc12f7e2ff3a3421
luimu64 <luimu@luimu.dev>
2023-08-25 06:50:11 +0300

    fully functional dropdown component

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -2,9 +2,29 @@ import { useState } from "react";
 
 const Dropdown = (props) => {
   const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState([]);
+
+  const handleMenuItemClick = (option) => {
+    //already selected -> remove from selected
+    if (selected.includes(option)) {
+      const newSelected = selected.filter((opt) => opt !== option);
+      //run the external value setter -> expose component internal values outside of it
+      if (props.setValue) props.setValue(newSelected);
+      setSelected(newSelected);
+    }
+
+    //not in the selected list and less than 4 selected -> add to selected
+    if (selected.length < props.numberToSelect && !selected.includes(option)) {
+      const newSelected = [...selected, option];
+      if (props.setValue) props.setValue(newSelected);
+      setSelected(newSelected);
+    }
+  };
 
   const styles = {
-    arrowUp: {},
+    arrowUp: {
+      transform: "rotate(180deg)",
+    },
     mainBox: {
       display: "flex",
       justifyContent: "space-between",
@@ -14,6 +34,7 @@ const Dropdown = (props) => {
       padding: ".4rem 1.5rem",
       fontFamily: "Montserrat, sans-serif",
       userSelect: "none",
+      height: "3.5rem",
     },
     title: {
       fontWeight: "200",
@@ -22,7 +43,6 @@ const Dropdown = (props) => {
     menu: {
       borderRadius: ".5rem",
       outline: ".1rem solid #CCCCCC",
-      padding: ".4rem 1.5rem",
       fontFamily: "Montserrat, sans-serif",
       position: "absolute",
       width: "100%",
@@ -31,6 +51,7 @@ const Dropdown = (props) => {
       maxHeight: "20rem",
       overflow: "scroll",
       backgroundColor: "white",
+      padding: "1rem 0",
     },
     menuClosed: {
       display: "none",
@@ -38,13 +59,29 @@ const Dropdown = (props) => {
     wrapper: {
       position: "relative",
     },
+    selectedText: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+    menuItem: {
+      padding: ".5rem 1.5rem",
+      margin: "0",
+      userSelect: "none",
+    },
+    selectedMenuItem: {
+      backgroundColor: "#120A2F",
+      color: "white",
+    },
   };
 
   return (
     <div style={styles.wrapper}>
       <h3 style={styles.title}>{props.title}</h3>
       <div style={styles.mainBox} onClick={() => setOpen(!open)}>
-        <p>Example selected item</p>
+        <p style={styles.selectedText}>
+          {selected.map((op) => op.label).join(", ")}
+        </p>
         <img
           style={open ? styles.arrowUp : {}}
           src="pictures/expand-more.png"
@@ -53,7 +90,17 @@ const Dropdown = (props) => {
       </div>
       <div style={open ? styles.menu : styles.menuClosed}>
         {props.options?.map((option, i) => (
-          <p key={i}>{option.label}</p>
+          <p
+            style={
+              selected.includes(option)
+                ? { ...styles.selectedMenuItem, ...styles.menuItem }
+                : styles.menuItem
+            }
+            onClick={() => handleMenuItemClick(option)}
+            key={i}
+          >
+            {option.label}
+          </p>
         ))}
       </div>
     </div>
