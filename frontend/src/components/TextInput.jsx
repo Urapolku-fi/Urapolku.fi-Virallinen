198e382378a1eaea0db26c8b805272ebcbddf8b9
luimu64 <luimu@luimu.dev>
2023-08-25 05:14:16 +0300

    added text input component

---
+++ frontend/src/components/TextInput.jsx
@@ -0,0 +1,32 @@
+const TextInput = (props) => {
+  const styles = {
+    textInput: {
+      width: "100%",
+      borderRadius: ".5rem",
+      border: "none",
+      outline: ".1rem solid #CCCCCC",
+      padding: "1.5rem",
+      fontSize: "1.1rem",
+      boxSizing: "border-box",
+      fontFamily: "Montserrat, sans-serif",
+      ...props.extraClass,
+    },
+    title: {
+      fontWeight: "200",
+      marginBottom: ".5rem",
+    },
+  };
+
+  return (
+    <div>
+      <h3 style={styles.title}>{props.title}</h3>
+      <input
+        type={props.type || "text"}
+        style={styles.textInput}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
