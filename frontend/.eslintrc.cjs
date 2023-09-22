b2bfafabc62f50070b1f0e98c395cda6e4842f7f
Adem Ottoman <50057725+Ademrobert@users.noreply.github.com>
2023-09-22 22:40:27 +0300

    Update .eslintrc.cjs
    Created a few more rule in ESlint.

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,10 +12,33 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    // Code Quality Guidelines
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "react/prop-types": [0]
+    "react/prop-types": [0], // Disabling prop-types rule for now
+
+    // Add more code quality and security rules as needed
+
+     'no-console': 'error', // Prevent the use of console.log, consider using proper logging
+
+    // Styling Guidelines
+    // You can add Prettier configuration here for automatic code formatting
+
+     'prettier/prettier': 'error',
+
+    // Accessibility Guidelines
+    // Ensure your code follows accessibility best practices
+
+    'jsx-a11y/alt-text': 'error', // Require alt text for images
+
+    // Security Guidelines
+    // Enforce security best practices
+    // Example:
+    // 'security/detect-non-literal-regexp': 'error', // Detect potentially unsafe regex patterns
+
+    // Additional Guidelines
+    // Add any other project-specific rules or configurations here
   },
 }
