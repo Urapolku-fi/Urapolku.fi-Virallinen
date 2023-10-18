e39d3fb0db524717ed960106496891d28c47fdf3
antoKeinanen <anto.keinanen@gmail.com>
2023-10-18 14:37:20 +0300

    feat: create hero section for favorites page

--- frontend/src/components/pages/favorites.jsx
+++ frontend/src/components/pages/favorites.jsx
@@ -4,6 +4,13 @@ import TextInput from '../TextInput';
 function Favorites() {
   return (
     <main>
+      <div className="hero-section">
+        <h1>Saved Jobs</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore
+        </p>
+      </div>
     </main>
   );
 }
