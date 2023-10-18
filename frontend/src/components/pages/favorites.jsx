45265f92c26890c9ce8a47f772b6569b6563d09f
antoKeinanen <anto.keinanen@gmail.com>
2023-10-18 19:27:15 +0300

    feat: create filter panel

--- frontend/src/components/pages/favorites.jsx
+++ frontend/src/components/pages/favorites.jsx
@@ -1,7 +1,111 @@
 import '../css/favorites.css';
+import Filter from './Browse/Filter';
+import { useState } from 'react';
 import TextInput from '../TextInput';
 
+const loremIpsum = [
+  ['hello'],
+  'lorem',
+  'ipsum',
+  'dolor',
+  'sit',
+  'amet',
+  'consectetur',
+  'adipiscing',
+  'elit',
+  'curabitur',
+  'vel',
+  'hendrerit',
+  'libero',
+  'eleifend',
+  'blandit',
+  'nunc',
+  'ornare',
+  'odio',
+  'ut',
+  ['damn'],
+  'orci',
+  'gravida',
+  'imperdiet',
+  'nullam',
+  'purus',
+  'lacinia',
+  'a',
+  'pretium',
+  'quis',
+  'congue',
+  'praesent',
+  'sagittis',
+  'laoreet',
+  'auctor',
+  'mauris',
+  'non',
+  'velit',
+  'eros',
+  ['world'],
+  'dictum',
+  'proin',
+  'accumsan',
+  'sapien',
+  'nec',
+  'massa',
+  'volutpat',
+  'venenatis',
+  'sed',
+  'eu',
+  'molestie',
+  'lacus',
+  'quisque',
+  'porttitor',
+  'ligula',
+  'dui',
+  'mollis',
+  'tempus',
+];
+
+const defaultFiltersState = {
+  location: [loremIpsum, Array(loremIpsum.filter((e) => typeof e === 'string').length).fill(false)],
+  jobType: [
+    ['Full Time', true],
+    ['Part Time', false],
+    ['Internship', false],
+    ['Apprenticeship', false],
+  ],
+  workType: [
+    ['On-site', true],
+    ['Hybrid', false],
+    ['Remote', false],
+    ['Custom hours', false],
+  ],
+  industry: [loremIpsum, Array(loremIpsum.filter((e) => typeof e === 'string').length).fill(false)],
+  company: [loremIpsum, Array(loremIpsum.filter((e) => typeof e === 'string').length).fill(false)],
+  language: [
+    ['English', true],
+    ['Swedish', false],
+    ['Finnish', false],
+  ],
+  role: [loremIpsum, Array(loremIpsum.filter((e) => typeof e === 'string').length).fill(false)],
+  salary: [0, 15000],
+  education: [
+    'Bachelor',
+    'Master',
+    'Licentiate',
+    'Doctor',
+    'Upper secondary school',
+    'Vocational school',
+    'Comprehensive school',
+    'No education',
+    'Certified course',
+  ],
+};
+
 function Favorites() {
+  const [filtersState, setFiltersState] = useState(defaultFiltersState);
+
+  const clearFilters = () => {
+    setFiltersState(defaultFiltersState);
+  };
+
   return (
     <main>
       <div className="hero-section">
@@ -11,6 +115,21 @@ function Favorites() {
           ut labore et dolore
         </p>
       </div>
+
+      <div className="filter-and-jobs-container">
+        <div>
+          <h2 className="text-job-count">{mocData.length} saved jobs</h2>
+          <div className="filter-container">
+            <Filter
+              filtersState={filtersState}
+              setFiltersState={setFiltersState}
+              clearFilters={clearFilters}
+            />
+          </div>
+        </div>
+
+          </div>
+        </div>
     </main>
   );
 }
