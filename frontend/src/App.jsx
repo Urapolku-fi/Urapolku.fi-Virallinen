3f0e3bbd5a44ad8f668997a4da83dca51d4df773
antoKeinanen <anto.keinanen@gmail.com>
2023-10-19 15:36:00 +0300

    ref: remove component test page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,6 @@ import { Auth0Provider, withAuthenticationRequired as withAuth } from '@auth0/au
 import AccountTypeSelection from './components/pages/AccountTypeSelection';
 import EmployerProfileCompletion from './components/pages/EmployerProfileCompletion';
 import JobDetails from './components/pages/JobDetails';
-import ComponentTest from './components/pages/ComponentTest';
 
 function App() {
   return (
@@ -36,7 +35,6 @@ function App() {
             <Route path="/emponboarding" Component={withAuth(EmployerProfileCompletion)} />
             <Route path="/dashboard" Component={withAuth(Dashboard)} />
             <Route exact path="/job" Component={withAuth(JobDetails)} />
-            <Route exact path="/component-test" Component={withAuth(ComponentTest)} />
           </Routes>
         </NavBarLayout>
       </BrowserRouter>
